9a83f0a50db204c221bc04ea9efb54fe3936bbd5
Aditya Korde <korde.aditya@gmail.com>
2020-09-05 18:56:38 +0530

    Configuration setup

--- BranchDemo/BranchDemo/AppDelegate.swift
+++ BranchDemo/BranchDemo/AppDelegate.swift
@@ -7,7 +7,7 @@
 //
 
 import UIKit
-
+import Branch
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
@@ -15,9 +15,30 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        // if you are using the TEST key
+        Branch.setUseTestBranchKey(true)
+        // listener for Branch Deep Link data
+        Branch.getInstance().initSession(launchOptions: launchOptions) { (params, error) in
+          // do stuff with deep link data (nav to page, display content, etc)
+          print(params as? [String: AnyObject] ?? {})
+        }
+        
         return true
     }
-
+    func application(_ app: UIApplication, open url: URL, options: [UIApplication.OpenURLOptionsKey : Any] = [:]) -> Bool {
+      Branch.getInstance().application(app, open: url, options: options)
+      return true
+    }
+    func application(_ application: UIApplication, didReceiveRemoteNotification userInfo: [AnyHashable : Any], fetchCompletionHandler completionHandler: @escaping (UIBackgroundFetchResult) -> Void) {
+      // handler for Push Notifications
+      Branch.getInstance().handlePushNotification(userInfo)
+    }
+    func application(_ application: UIApplication, continue userActivity: NSUserActivity, restorationHandler: @escaping ([UIUserActivityRestoring]?) -> Void) -> Bool {
+      // handler for Universal Links
+      Branch.getInstance().continue(userActivity)
+      return true
+    }
+    
     // MARK: UISceneSession Lifecycle
 
     func application(_ application: UIApplication, configurationForConnecting connectingSceneSession: UISceneSession, options: UIScene.ConnectionOptions) -> UISceneConfiguration {
